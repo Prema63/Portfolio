edf789d4234693764706454510a5900544ec6af4
Prema63 <darkshadowprema@gmail.xom>
2025-12-25 21:29:38 +0530

    minor changes

--- src/portfoliopage/Mainpage.jsx
+++ src/portfoliopage/Mainpage.jsx
@@ -76,8 +76,8 @@ const Portfolio = () => {
   const experiences = [
     {
       company: "NK Tech",
-      position: "Web Developer(MERN Stack)",
-      duration: "Sep 2025 - Present",
+      position: "Web Developer(MERN Stack) Intern",
+      duration: "September 2025 - December 2025",
       achievements: [
         "Creating responsive and interactive user interfaces with React.js and Next.js.",
         "Supporting backend development with Node.js and Express.js.",
@@ -88,11 +88,11 @@ const Portfolio = () => {
     {
       company: "Encrobytes Technologies",
       position: "MERN Stack Intern",
-      duration: "April 2025 - Aug 2025",
+      duration: "April 2025 - August 2025",
       achievements: [
         "Develop scalable and efficient web applications using MERN stack technologies.",
         "Design and implement RESTful APIs using Node.js",
-        " Build responsive and interactive user interfaces using Next.js projects",
+        "Build responsive and interactive user interfaces using Next.js projects",
         "Collaborate with team members to design and implement database schema using MongoDB.",
         "Participate in code reviews, testing, and debugging.",
       ],
@@ -500,108 +500,6 @@ const Portfolio = () => {
       <div className="h-1 bg-gradient-to-b from-blue-800 via-purple-800 to-transparent"></div>
 
       {/* Projects Section */}
-      {/* <section
-        id="projects"
-        className="py-20 px-4 sm:px-6 lg:px-8 bg-gray-800 bg-gradient-to-b from-gray-900 via-indigo-950 to-gray-900 text-gray-100"
-      >
-        <div className="max-w-7xl mx-auto">
-          <div
-            className={`transition-all duration-1000 ${
-              isVisible.projects
-                ? "opacity-100 translate-y-0"
-                : "opacity-0 translate-y-10"
-            }`}
-          >
-            <h2 className="text-3xl sm:text-4xl font-bold text-gray-400 text-center mb-16">
-              Featured <span className="text-blue-400">Projects</span>
-            </h2>
-
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {projects?.map((project, index) => (
-                <div
-                  key={index}
-                  className="bg-gray-900 rounded-lg overflow-hidden hover:transform hover:scale-105 transition-all duration-300"
-                >
-                  <div className="h-48 w-full overflow-hidden">
-                    {project.img ? (
-                      <img
-                        src={project.img}
-                        alt={project.title}
-                        className="w-full h-full object-cover"
-                      />
-                    ) : (
-                      <div
-                        className={`h-full ${project.img} flex items-center justify-center`}
-                      >
-                      </div>
-                    )}
-                  </div>
-
-                  <div className="p-6">
-                    <h3 className="text-xl font-semibold mb-3">
-                      {project.title}
-                    </h3>
-                    <p className="text-gray-300 text-sm mb-4">
-                      {project.description}
-                    </p>
-
-                    <div className="mb-4">
-                      <h4 className="font-medium mb-2 text-purple-400">
-                        Technologies:
-                      </h4>
-                      <div className="flex flex-wrap gap-2">
-                        {project.tech.map((tech, techIndex) => (
-                          <span
-                            key={techIndex}
-                            className="px-2 py-1 bg-gray-700 text-xs rounded-md"
-                          >
-                            {tech}
-                          </span>
-                        ))}
-                      </div>
-                    </div>
-
-                    <div className="mb-6">
-                      <h4 className="font-medium mb-2 text-green-400">
-                        Features:
-                      </h4>
-                      <ul className="text-sm space-y-1">
-                        {project.features.map((feature, featIndex) => (
-                          <li
-                            key={featIndex}
-                            className="flex items-center gap-2"
-                          >
-                            <div className="w-1 h-1 bg-green-400 rounded-full"></div>
-                            {feature}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-
-                    <div className="flex gap-4">
-                      <a
-                        href={project.github}
-                        className="flex items-center gap-2 text-sm bg-gray-700 px-4 py-2 rounded-lg hover:bg-gray-600 transition-colors"
-                      >
-                        <Github size={16} />
-                        Code
-                      </a>
-                      <a
-                        href={project.live}
-                        className="flex items-center gap-2 text-sm bg-blue-600 px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors"
-                      >
-                        <ExternalLink size={16} />
-                        Live Demo
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </section> */}
-
       <section
         id="projects"
         className="py-20 px-4 sm:px-6 lg:px-8 bg-gray-800 bg-gradient-to-b from-gray-900 via-indigo-950 to-gray-900 text-gray-100"
